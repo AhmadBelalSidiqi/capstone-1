555c390a4ba2d0950730c9442662b4d59c9b1b60
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-27 10:46:24 -0400

    ledger method  and displayAllTransaction added.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,7 @@ public class Main {
     static ArrayList<Transaction> transactions = new ArrayList<>();
     public static void main(String[] args) {
         homeScreenMenu();
-        displayAllTransaction();
+        displayAllTransactions();
 
     }
 
@@ -58,7 +58,7 @@ public class Main {
                 running=false; }
         }while (running);
     }
-    // TODO: Make the makePayments method
+
     public static void makePayments() {
         boolean running = true;
         do {
@@ -77,9 +77,44 @@ public class Main {
                 running=false; }
         }while (running);
     }
-    // TODO: Make the ledger method
     public static void ledger() {
+        String menu = """
+                Please choose one of the following option:
+                A) ALL- Display all entries
+                D) Deposits- Display only the deposits into the account
+                P) Payments- Display only the payments into the account
+                R) Report
+                """;
+        boolean running = true;
+        do { System.out.println(menu);
+            String input = scanner.nextLine().trim();
+            switch (input.toLowerCase()){
+                case "a"->
+                        displayAllTransactions();
+                case "d"->
+                        displayDeposits();
+                case "p"->
+                        displayPayments();
+                case "r"->
+                        report();
+                case "x"->
+                        running = false;
+                default -> System.out.println("Wrong Input");
+            }
+
+        }while (running);
+
     }
+    //TODO: Display the negative amounts
+    public static void displayPayments() {
+    }
+    // TODO: Display the positive amounts
+    public static void displayDeposits() {
+    }
+    // TODO: Make customized reports
+    public static void report() {
+    }
+
     public static void displayTransaction(Transaction transaction){
         System.out.printf("Date: %s\t Description: %s\t Vendor: %s\t Amount: %.2f %n",
                 transaction.getDateAndTimeFormated(),
@@ -89,7 +124,7 @@ public class Main {
 
     }
 
-    public static void displayAllTransaction(){
+    public static void displayAllTransactions(){
         for (Transaction transaction : transactions){
             displayTransaction(transaction);
         }
