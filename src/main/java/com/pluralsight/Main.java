23619975eb03c412d371665fe9f0c3bcb9a1167b
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-27 14:38:41 -0400

    created method display pre-defined report on the terminal.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,13 +1,17 @@
 package com.pluralsight;
 
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static LocalDateTime currentDateAndTime = LocalDateTime.now();
     static Scanner scanner = new Scanner(System.in);
     static ArrayList<Transaction> transactions = loadOldTransactions();
     public static void main(String[] args) {
@@ -104,7 +108,8 @@ public class Main {
                         report();
                 case "x"->
                         running = false;
-                default -> System.out.println("Wrong Input");
+                default ->
+                        System.out.println("Wrong Input");
             }
 
         }while (running);
@@ -125,8 +130,106 @@ public class Main {
             }
         }
     }
-    // TODO: Make customized reports
+    // TODO: Going back to home screen
     public static void report() {
+        String menu = """
+                Please choose one of the following:
+                1) Month to Date
+                2) Previous Month
+                3) Year to Date
+                4) Previous Year
+                5) Search by Vendor
+                0) Go back to ledger Page
+                H) Home Screen
+                """;
+        boolean running = true;
+        do {
+            System.out.println(menu);
+            String input = scanner.nextLine();
+            switch (input.toLowerCase()){
+
+
+                case "1" ->
+                        monthToDate();
+
+                case "2"->
+                        previousMonth();
+
+                case "3"->
+                        yearToDate();
+
+                case "4" ->
+                        previousYear();
+
+                case "5"->
+                        searchByVendor();
+
+                case "0"->
+                        running=false;
+
+                case "h"->
+                        homeScreenMenu();
+
+                default ->
+                        System.out.println("wrong Input");
+
+            }
+        }while (running);
+
+    }
+
+    public static void monthToDate() {
+        LocalDate month = LocalDate.of(currentDateAndTime.getYear(),currentDateAndTime.getMonth(),1);
+        LocalTime time = LocalTime.of(1,1,1);
+        LocalDateTime thisMonth = LocalDateTime.of(month,time);
+        for (Transaction transaction : transactions){
+            if (transaction.getDateAndTime().isAfter(thisMonth)){
+                displayTransaction(transaction);
+            }
+        }
+
+    }
+
+    public static void previousMonth() {
+        LocalTime time = LocalTime.of(1,1);
+        LocalDate lastMonth = LocalDate.of(currentDateAndTime.getYear(),currentDateAndTime.getMonth().minus(1),1);
+        LocalDate month = LocalDate.of(currentDateAndTime.getYear(),currentDateAndTime.getMonth(),1);
+        LocalDateTime previousMonth = LocalDateTime.of(lastMonth,time);
+        LocalDateTime thisMonth = LocalDateTime.of(month,time);
+        for (Transaction transaction : transactions){
+            if (transaction.getDateAndTime().isAfter(previousMonth) && transaction.getDateAndTime().isBefore(thisMonth)){
+                displayTransaction(transaction);
+            }
+        }
+    }
+
+    public static void yearToDate() {
+        for (Transaction transaction : transactions){
+            // every transaction in this year and before current date and time , we want to avoid transaction that would be place in the future of current date and time.
+            if (transaction.getDateAndTime().getYear() == currentDateAndTime.getYear() && transaction.getDateAndTime().isBefore(currentDateAndTime)){
+                displayTransaction(transaction);
+            }
+        }
+
+    }
+
+    public static void previousYear() {
+        int previousYear = currentDateAndTime.getYear()-1;
+        for (Transaction transaction : transactions){
+            if (transaction.getDateAndTime().getYear() == previousYear){
+                displayTransaction(transaction);
+            }
+        }
+    }
+
+    public static void searchByVendor() {
+        System.out.println("Please enter the vendor name: ");
+        String vendor = scanner.nextLine();
+        for (Transaction transaction : transactions){
+            if(transaction.getVendor().equalsIgnoreCase(vendor)){
+                displayTransaction(transaction);
+            }
+        }
     }
 
     public static void displayTransaction(Transaction transaction){
@@ -171,6 +274,9 @@ public class Main {
             System.out.println("IO Exception: "+ e);
         }
         return oldTransactions;
+    }
+    public static void sortTransactionArray(){
+
     }
 
 
